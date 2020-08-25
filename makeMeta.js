6aeab6bdb6e085c8c7a00586c89f6034c4aeffd7
dev-mehmood <dev.mehmood@gmail.com>
2020-08-25 21:02:25 +0500

    changed file name

---
+++ makeMeta.js
@@ -0,0 +1,28 @@
+//requiring path and fs modules
+const path = require('path');
+const promisify = require('promisify-node')
+const fs = promisify('fs');
+const { exec, spawn, fork, execFile } = require('promisify-child-process');
+const fileName = '@dev-box-styleguide.js'
+async function exec_it() {
+
+    try {
+        const { stdout, stderr } = await exec('npm run build');
+
+        const directoryPath = path.join(__dirname, 'dist');
+
+        const files = await fs.readdir(directoryPath);
+
+        file = files.filter((f)=>f.endsWith(fileName))
+        if(file.length) {
+            hash = file[0].split('-')[0]
+            await fs.writeFile(`${directoryPath}/meta.txt`, hash)
+            console.log(file)
+        }
+       
+    } catch (e) {
+        throw (e)
+    }
+
+}
+exec_it()
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,15 @@ const singleSpaDefaults = require("webpack-config-single-spa");
 
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
-    orgName: "@meh-dev",
+    orgName: "@dev-box",
     projectName: "styleguide",
     webpackConfigEnv,
   });
 
   return webpackMerge.smart(defaultConfig, {
+    output:{
+      filename:`[hash]-@dev-box-styleguide.js`
+    }
     // modify the webpack config however you'd like to by adding to this object
   });
 };
