107bfc6456256f9ab4c068cbebdbd603a118eb64
dev-mehmood <dev.mehmood@gmail.com>
2020-10-14 14:24:01 +0500

    fixing some bugs

--- src/styleguide.js
+++ src/styleguide.js
@@ -1,4 +1,4 @@
-import "./set-public-path";
+// import "./set-public-path";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Anything exported from this file is importable by other in-browser modules.
